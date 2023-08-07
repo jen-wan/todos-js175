490f24e1fcc6a73fca72d76381a2427e0aadbf60
jen-wan <jennyjwan2@gmail.com>
2023-08-07 12:36:34 -0700

    save app

--- todos.js
+++ todos.js
@@ -5,13 +5,17 @@ const app = express(); // create the Express application object, app
 const host = "localhost";
 const port = 3000;
 
+// Static data for initial testing
+let todoLists = require("./lib/seed-data");
+
 app.set("views", "./views");
 app.set("view engine", "pug");
 
 app.use(morgan("common"));
+app.use(express.static("public")); // tells Express to find static assets in public directory
 
 app.get("/", (req, res) => { // primary route for application
-  res.render("lists");
+  res.render("lists", { todoLists });
 });
 
 // Listener
