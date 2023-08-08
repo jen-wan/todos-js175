ee548fee564099f0311c6c2109709a7a10f57818
jen-wan <jennyjwan2@gmail.com>
2023-08-08 12:47:32 -0700

    save project

--- todos.js
+++ todos.js
@@ -3,6 +3,7 @@ const express = require("express");
 const morgan = require("morgan");
 const flash = require("express-flash");
 const session = require("express-session");
+const { body, validationResult } = require("express-validator");
 const TodoList = require("./lib/todolist");
 
 const app = express(); // create the Express application object, app
@@ -30,7 +31,8 @@ app.use(session({
 // Adds flash messages middlware to our Express.js app.
 app.use(flash());
 
-// Extract session info
+// Extract session info so that flash messages before redirect are available
+// for use in views rendered by res.render.
 app.use((req, res, next) => {
   res.locals.flash = req.session.flash; // extract flash messages and place in res.locals.
   delete req.session.flash;
@@ -75,33 +77,36 @@ app.get("/lists/new", (req, res) => {
   res.render("new-list");
 });
 
-// Create a new todo List
-app.post("/lists", (req, res) => {
-  let title = req.body.todoListTitle.trim(); // Get and trim the title from the request body
-  // If title is empty
-  if (title.length === 0) {
-    req.flash("error", "A title was not provided.");
-    res.render("new-list", { flash: req.flash() });
-  } else if (title.length > 100) {
-    req.flash("error", "List title must be between 1 and 100 characters.");
-    req.flash("error", "This is another error."); // to demonstrate that our app manages multiple error messages correctly.
-    req.flash("error", "Here is still another error.");
-    res.render("new-list", {
-      flash: req.flash(),
-      todoListTitle: req.body.todoListTitle,
-    });
-  } else if (todoLists.some(list => list.title === title)) {
-    req.flash("error", "List title must be unique.");
-    res.render("new-list", {
-      flash: req.flash(),
-      todoListTitle: req.body.todoListTitle,
-    });
-  } else {
-    todoLists.push(new TodoList(title)); // Create new todo list with the title & add new todo list to list of todo lists
-    req.flash("success", "The todo list has been created.");
-    res.redirect("/lists"); // redirects back to get version of /lists route
+// Create a new todo List, using express-validator library for form validation
+app.post("/lists", // sets up route for handling POST requests to "/lists" URL endpoint
+  [
+    body("todoListTitle") // targets the todoListTitle field in request body and applies validation checks to it.
+      .trim()
+      .isLength({ min: 1 })
+      .withMessage("The list title is required.") // error message is attached if validation check fails.
+      .isLength({ max: 100 })
+      .withMessage("List title must be between 1 and 100 characters.")
+      .custom(title => {
+        let duplicate = todoLists.find(list => list.title === title);
+        return duplicate === undefined;
+      })
+      .withMessage("List title must be unique."),
+  ],
+  (req, res) => { // Defines the error handler function that is executed when "/lists" route is accessed with Post request.
+    let errors = validationResult(req); // Uses express-validator library to gather results of validation checks performed on the request.
+    if (!errors.isEmpty()) { // Checks if there are any validation errors.
+      errors.array().forEach(message => req.flash("error", message.msg)); // adds each validation error message to flash message with key "error".
+      res.render("new-list", { // Renders "new-list" template with flash messages and previously submitted title.
+        flash: req.flash(),
+        todoListTitle: req.body.todoListTitle,
+      });
+    } else { // Primary callback for route: If no validation errors, add new instance of TodoList class to todoLists array.
+      todoLists.push(new TodoList(req.body.todoListTitle));
+      req.flash("success", "The todo list has been created."); // success flash message
+      res.redirect("/lists"); // The response redirects user to "/lists" URL after successfully creating todo list.
+    }
   }
-});
+);
 
 // Listener
 app.listen(port, host, () => {
